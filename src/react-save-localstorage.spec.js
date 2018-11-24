ba974c083774cdd2d0e2cec0b8b455f1661f53a3
Antony Budianto <antonybudianto@gmail.com>
2018-11-24 16:50:49 +0700

    fix test for node 8

--- src/react-save-localstorage.spec.js
+++ src/react-save-localstorage.spec.js
@@ -2,28 +2,6 @@ import React from 'react';
 import { render, cleanup } from 'react-testing-library';
 import SaveLocalStorage from './react-save-localstorage';
 
-const localStorageMock = (function() {
-  let store = {};
-  return {
-    getItem: function(key) {
-      return store[key] || null;
-    },
-    setItem: jest.fn(function(key, value) {
-      store[key] = value.toString();
-    }),
-    removeItem: function(key) {
-      delete store[key];
-    },
-    clear: function() {
-      store = {};
-    }
-  };
-})();
-
-Object.defineProperty(window, 'localStorage', {
-  value: localStorageMock
-});
-
 afterEach(cleanup);
 afterEach(() => {
   localStorage.clear();
@@ -37,7 +15,7 @@ describe('SaveLocalStorage', () => {
     render(<SaveLocalStorage lsKey="email" value="hello" />);
     email = localStorage.getItem('email');
     expect(email).toBe('hello');
-    expect(localStorageMock.setItem).toHaveBeenCalledTimes(1);
+    expect(localStorage.setItem).toHaveBeenCalledTimes(1);
   });
 
   it('should save data from render - rerender with same value', async () => {
@@ -49,7 +27,7 @@ describe('SaveLocalStorage', () => {
     email = localStorage.getItem('email');
     expect(email).toBe('hello');
     rerender(<SaveLocalStorage lsKey="email" value="hello" />);
-    expect(localStorageMock.setItem).toHaveBeenCalledTimes(1);
+    expect(localStorage.setItem).toHaveBeenCalledTimes(1);
   });
 
   it('should save data from render - rerender with same value', async () => {
@@ -63,7 +41,7 @@ describe('SaveLocalStorage', () => {
     rerender(<SaveLocalStorage lsKey="email" value="world" />);
     email = localStorage.getItem('email');
     expect(email).toBe('world');
-    expect(localStorageMock.setItem).toHaveBeenCalledTimes(2);
+    expect(localStorage.setItem).toHaveBeenCalledTimes(2);
   });
 
   it('should not save data from render - with empty value', async () => {
@@ -72,6 +50,6 @@ describe('SaveLocalStorage', () => {
     render(<SaveLocalStorage lsKey="email" value="" />);
     email = localStorage.getItem('email');
     expect(email).toBe(null);
-    expect(localStorageMock.setItem).toHaveBeenCalledTimes(0);
+    expect(localStorage.setItem).toHaveBeenCalledTimes(0);
   });
 });
