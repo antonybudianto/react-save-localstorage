062282407f5b71d7f7720f63c8ced3d66cfa8b73
Antony Budianto <antonybudianto@gmail.com>
2018-11-24 16:53:21 +0700

    Enhance test (#1)
    * eslint: use env browser and add jest
    
    * add unit test react-save-localstorage component
    
    * add unit test localstorage util on node env
    
    * fix test case with same value
    
    * fix test for node 8

---
+++ src/react-save-localstorage.spec.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import { render, cleanup } from 'react-testing-library';
+import SaveLocalStorage from './react-save-localstorage';
+
+afterEach(cleanup);
+afterEach(() => {
+  localStorage.clear();
+  localStorage.setItem.mockClear();
+});
+
+describe('SaveLocalStorage', () => {
+  it('should save data from render - normal case', async () => {
+    let email = localStorage.getItem('email');
+    expect(email).toBeNull();
+    render(<SaveLocalStorage lsKey="email" value="hello" />);
+    email = localStorage.getItem('email');
+    expect(email).toBe('hello');
+    expect(localStorage.setItem).toHaveBeenCalledTimes(1);
+  });
+
+  it('should save data from render - rerender with same value', async () => {
+    let email = localStorage.getItem('email');
+    expect(email).toBeNull();
+    const { rerender } = render(
+      <SaveLocalStorage lsKey="email" value="hello" />
+    );
+    email = localStorage.getItem('email');
+    expect(email).toBe('hello');
+    rerender(<SaveLocalStorage lsKey="email" value="hello" />);
+    expect(localStorage.setItem).toHaveBeenCalledTimes(1);
+  });
+
+  it('should save data from render - rerender with same value', async () => {
+    let email = localStorage.getItem('email');
+    expect(email).toBeNull();
+    const { rerender } = render(
+      <SaveLocalStorage lsKey="email" value="hello" />
+    );
+    email = localStorage.getItem('email');
+    expect(email).toBe('hello');
+    rerender(<SaveLocalStorage lsKey="email" value="world" />);
+    email = localStorage.getItem('email');
+    expect(email).toBe('world');
+    expect(localStorage.setItem).toHaveBeenCalledTimes(2);
+  });
+
+  it('should not save data from render - with empty value', async () => {
+    let email = localStorage.getItem('email');
+    expect(email).toBeNull();
+    render(<SaveLocalStorage lsKey="email" value="" />);
+    email = localStorage.getItem('email');
+    expect(email).toBe(null);
+    expect(localStorage.setItem).toHaveBeenCalledTimes(0);
+  });
+});
